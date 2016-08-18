3e4af11807c6f8185296d5a217329c3a40915a5f
Melissa Martinez <melmar12@live.com>
2016-08-18 15:28:05 -0500

    lesson 53: setting up API

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'get groceries',
+	completed: false
+}, {
+	id: 2,
+	description: 'go to yoga class',
+	completed: false
+}, {
+	id: 3,
+	description: 'drop off Ivy',
+	completed: true
+}
+
+]
 
 app.get('/', function (req, res) {
 	res.send('ToDo API Root')
 });
+
+// GET /todos/
+
+app.get('/todos', function (req, res) {
+	res.json(todos);
+});
+
+// GET /todos/:id
+
 
 app.listen(PORT, function () {
 	console.log('express listening on port ' + PORT + '!');
