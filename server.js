e710c0e404d140f115117e180d81f2842a7174fb
Melissa Martinez <melmar12@live.com>
2016-08-20 17:28:01 -0500

    lesson 58: added validations with underscore

--- server.js
+++ server.js
@@ -33,9 +33,11 @@ app.get('/todos/:id', function (req, res) {
 
 // POST /todos
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
 
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	body.description = body.description.trim();
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.length === 0) {
 		return res.status(400).send();
 	}
 
