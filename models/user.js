0a5ca02c4365cd4536a4ebcc1abd250126bea636
Melissa Martinez <melmar12@live.com>
2016-10-07 15:50:58 -0500

    lesson 78: add Auth token to POST/ users

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 var bcrypt = require('bcryptjs');
 var _ = require('underscore');
+var crypto = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
 module.exports = function(sequelize, DataTypes) {
 	var user = sequelize.define('user', {
@@ -43,7 +45,7 @@ module.exports = function(sequelize, DataTypes) {
 		classMethods: {
 			authenticate: function(body) {
 
-				return new Promise( function (resolve, reject) { 
+				return new Promise(function(resolve, reject) {
 
 					if (typeof body.email !== 'string' || typeof body.password !== 'string') {
 						return reject();
@@ -71,9 +73,26 @@ module.exports = function(sequelize, DataTypes) {
 				var json = this.toJSON();
 
 				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+			},
+			generateToken: function(type) {
+				if (!_.isString(type)) {
+					return undefined
+				}
+
+				try {
+					var stringData = JSON.stringify({id: this.get('id'), type: type});
+					var encryptedData = crypto.AES.encrypt(stringData, 'abc123!@#').toString();
+					var token = jwt.sign({
+						token: encryptedData
+					}, 'qwerty098');
+
+					return token;
+				} catch (e) {
+
+				}
 			}
 		}
 	});
 
-return user;
+	return user;
 };
