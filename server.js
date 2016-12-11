12c0b7b5c5346a5ef45e7c6d20cd0a6a89e3d53b
Melissa Martinez <melmar12@live.com>
2016-12-11 14:05:51 -0600

    lesson 82: convert CRUD requests to use current user todos only

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ app.get('/', function(req, res) {
 // GET /todos/
 app.get('/todos', middleware.requireAuthentication, function(req, res) {
 	var query = req.query;
-	var where = {};
+	var where = {
+		userId: req.user.get('id')
+	};
 
 	if (query.hasOwnProperty('completed') && query.completed === 'true') {
 		where.completed = true;
@@ -45,9 +47,14 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 
 // GET /todos/:id
 app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
-	var todoID = parseInt(req.params.id, 10);
+	var todoId = parseInt(req.params.id, 10);
 
-	db.todo.findById(todoID).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (!!todo) {
 			res.json(todo.toJSON());
 		} else {
@@ -79,7 +86,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 
 	db.todo.destroy({
 		where: {
-			id: todoID
+			id: todoID,
+			userId: req.user.get('id')
 		}
 	}).then(function(rowsDeleted) {
 		if (rowsDeleted === 0) {
@@ -98,7 +106,7 @@ app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 
 // PUT
 app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
-	var todoID = parseInt(req.params.id, 10);
+	var todoId = parseInt(req.params.id, 10);
 
 	var body = _.pick(req.body, 'description', 'completed');
 	var atributes = {};
@@ -112,7 +120,12 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 		atributes.description = body.description;
 	}
 
-	db.todo.findById(todoID).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (todo) {
 			todo.update(atributes).then(function(todo) {
 				res.json(todo.toJSON());
